dfc26d7cbe84fbc4c6377ac98a678114ba85115e
Gabriel Dias <diasgab@gmail.com>
2022-12-14 12:53:58 +0100

    Add Cronofy client provider

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Cronofy Provider for OAuth 2.0 Client
 
 Cronofy OAuth 2.0 Client Provider for The PHP League OAuth2-Client
+
+## Installation
+
+To install the package, use composer:
+
+```
+composer require diasgab/oauth2-cronofy
+```
---
+++ src/Provider/Cronofy.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Diasgab\OAuth2\Client\Provider;
+
+use ApiBundle\Provider\Exception\CronofyIdentityProviderException;
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+
+class Cronofy extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return 'https://app.cronofy.com/oauth/authorize';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param array $params
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return 'https://api.cronofy.com/oauth/token';
+    }
+
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return 'https://api.cronofy.com/v1/userinfo';
+    }
+
+    protected function fetchResourceOwnerDetails(AccessToken $token)
+    {
+        $response = parent::fetchResourceOwnerDetails($token);
+
+        return $response;
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    /**
+     * Check a provider response for errors.
+     *
+     * @param ResponseInterface $response
+     * @param array $data Parsed response data
+     * @return void
+     * @throws IdentityProviderException
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400) {
+            throw CronofyIdentityProviderException::clientException($response, $data);
+        } elseif (isset($data['error'])) {
+            throw CronofyIdentityProviderException::oauthException($response, $data);
+        }
+    }
+
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     * @return \League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new CronofyResourceOwner($response);
+    }
+}
